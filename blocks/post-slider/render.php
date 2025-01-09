8051ca44efc76e532be00ef8f13ed2c3b84a4f3a
Kirby Johnston <johnstk91@gmail.com>
2025-01-09 14:05:38 -0500

    install and set up blaze slider for post slider block

--- blocks/post-slider/render.php
+++ blocks/post-slider/render.php
@@ -10,27 +10,33 @@
 		'post_type' => $attributes["selectedPostType"]
 	]);
 ?>
-<div class="post-slider">
-	<ul>
-		<?php foreach ($posts as $post): 
-			$featured_image_url = get_the_post_thumbnail_url($post);
-			$image_url = $featured_image_url ?: get_option("fallback_image");
+<div class="post-slider blaze-slider">
+	<div class="blaze-container">
+		<div class="blaze-track-container">
+			<ul class="blaze-track">
+			<?php foreach ($posts as $post): 
+				$featured_image_url = get_the_post_thumbnail_url($post);
+				$image_url = $featured_image_url ?: get_option("fallback_image");
 
-			$permalink = get_the_permalink($post);
-			?>
-			<li>
-				<a class="post-card" href="<?= $permalink; ?>">
-					<figure>
-						<?php if (!empty($image_url)): ?>
-							<img src="<?= $image_url ?>" alt="<?= $post->title; ?>">
-						<?php endif; ?>
+				$permalink = get_the_permalink($post);
+				?>
 
-						<figcaption>
-							<?= $post->post_excerpt ?: $post->post_title; ?>
-						</figcaption>
-					</figure>
-				</a>
-			</li>
-		<?php endforeach; ?>
-	</ul>
+				<li>
+					<a class="post-card" href="<?= $permalink; ?>">
+						<figure>
+							<?php if (!empty($image_url)): ?>
+								<img src="<?= $image_url ?>" alt="<?= $post->title; ?>">
+							<?php endif; ?>
+
+							<figcaption>
+								<?= $post->post_excerpt ?: $post->post_title; ?>
+							</figcaption>
+						</figure>
+					</a>
+				</li>
+				
+			<?php endforeach; ?>
+			</ul>
+		</div>
+	</div>
 </div>
